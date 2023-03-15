7c00028f9e53fa61f207dd38cefba2034ebb19cd
a.zanetti6 <a.zanetti6@studenti.unibg.it>
2023-03-15 17:46:37 +0100

    Adds BookshelfObject enum
    This enum holds the instances of Bookshelfobjects available in the game.
    The class makes available a static method to return a random
    BookshelfObject form the enum.
    To comply with the game rules, the number of instances of every object
    must be checked. My Shelfy allows 22 instances of the same object.
    If the randomly selected object is not available, the next object is
    selected.
    
    Signed-off-by: a.zanetti6 <a.zanetti6@studenti.unibg.it>

---
+++ src/myshelfie/BookshelfObject.java
@@ -0,0 +1,57 @@
+package myshelfie;
+
+import java.util.Random;
+
+public enum BookshelfObject
+{
+	CAT(),
+	BOOK(),
+	GAME(),
+	FRAME(),
+	TROPHY(),
+	PLANT();
+
+	public static final int MAX_INSTANCES = 22;
+	private static final Random RANDOM = new Random();
+
+	private int instancesCounter;
+
+	private BookshelfObject()
+	{
+		instancesCounter = 0;
+	}
+
+	private boolean reachedMaxInstances()
+	{
+		return (this.instancesCounter >= MAX_INSTANCES) ? true : false;
+	}
+
+	/**
+	 * Returns a random ShelfObject if its instances number is less than the max limit.
+	 * If this method returns null it means that there are no more ShelfObject available.
+	 *
+	 * @return	a random available ShelfObject
+	 */
+	public static BookshelfObject getRandomObject()
+	{
+		BookshelfObject object = null;
+		BookshelfObject[] objects = values();
+		int randomIndex = RANDOM.nextInt(objects.length);
+		int indexCounter = 0;
+
+		do
+		{
+			object = objects[randomIndex];
+			randomIndex++;
+			indexCounter++;
+
+			if(randomIndex >= objects.length)
+			{
+				randomIndex = 0;
+			}
+		} while(object.reachedMaxInstances() && indexCounter < objects.length);
+
+		object.instancesCounter++;
+		return object;
+	}
+}
