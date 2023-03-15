f20bc712186e18d591a4587e55ff0141697e32b8
a.zanetti6 <a.zanetti6@studenti.unibg.it>
2023-03-15 17:44:08 +0100

    Adds TileType enum
    This enum holds the instances of the different types of tile that compose
    the board in My Shelfy.
    
    Signed-off-by: a.zanetti6 <a.zanetti6@studenti.unibg.it>

---
+++ src/myshelfie/TileType.java
@@ -0,0 +1,20 @@
+package myshelfie;
+
+public enum TileType
+{
+	TWO_DOTS(2),
+	THREE_DOTS(3),
+	FOUR_DOTS(4);
+
+	private int playersNumber;
+
+	private TileType(int playersNumber)
+	{
+		this.playersNumber = playersNumber;
+	}
+
+	public int getPlayersNumber()
+	{
+		return playersNumber;
+	}
+}
