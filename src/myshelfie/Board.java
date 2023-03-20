2c2efd8b5e9b863eae0d234dcd8d8a87f93d78d5
a.zanetti6 <a.zanetti6@studenti.unibg.it>
2023-03-20 18:53:56 +0100

    Adds Board class
    This class represent a generic board for the game.
    It contains a read-only tiles table that define the shape of the board.
    It also contains a method to initialize the board, and a method to
    randomly select the shelf-objects and place them in the right tiles
    for the selected players number.
    
    Signed-off-by: a.zanetti6 <a.zanetti6@studenti.unibg.it>

---
+++ src/myshelfie/Board.java
@@ -0,0 +1,44 @@
+package myshelfie;
+
+public class Board
+{
+	private final int playersNumber;
+
+	public Board(int playerNumber)
+	{
+		this.playersNumber = playerNumber;
+	}
+
+	/* Helper functions */
+	private Tile tile2p() { return new Tile(TileType.TWO_DOTS); }
+	private Tile tile3p() { return new Tile(TileType.THREE_DOTS); }
+	private Tile tile4p() { return new Tile(TileType.FOUR_DOTS); }
+
+	private final Tile[][] livingroom =
+    {
+    	{null,	   null,	 null,	   tile3p(), tile4p(), null,     null,     null,     null    },
+    	{null, 	   null,     null,     tile2p(), tile2p(), tile4p(), null,     null,     null    },
+    	{null, 	   null,     tile3p(), tile2p(), tile2p(), tile2p(), tile3p(), null,     null    },
+    	{null, 	   tile4p(), tile2p(), tile2p(), tile2p(), tile2p(), tile2p(), tile2p(), tile3p()},
+    	{tile4p(), tile2p(), tile2p(), tile2p(), tile2p(), tile2p(), tile2p(), tile2p(), tile4p()},
+    	{tile3p(), tile2p(), tile2p(), tile2p(), tile2p(), tile2p(), tile2p(), tile4p(), null    },
+    	{null,     null,     tile3p(), tile2p(), tile2p(), tile2p(), tile3p(), null,     null    },
+    	{null,     null,     null,     tile4p(), tile2p(), tile2p(), null,     null,     null    },
+    	{null,     null,     null,     null,     tile4p(), tile3p(), null,     null,     null    },
+    };
+
+	public void FillLivingroomWithObjects()
+	{
+		for(int row = 0; row < livingroom.length; row++)
+			for(int col = 0; col < livingroom[0].length; col++)
+			{
+				Tile livingroomTile = livingroom[row][col];
+
+				if (livingroomTile != null && livingroomTile.isUsable(playersNumber))
+				{
+					BookshelfObject randomObject = BookshelfObject.getRandomObject();
+					livingroomTile.addObject(randomObject);
+				}
+			}
+	}
+}
