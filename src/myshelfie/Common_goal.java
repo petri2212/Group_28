6febb68c3ce037a3aee3111a851931fac4bae657
Andrei <a.petrisor@studenti.unibg.it>
2023-03-20 19:04:40 +0100

    Add Common_goal.java and start coding

--- src/myshelfie/Common_goal.java
+++ src/myshelfie/Common_goal.java
@@ -15,12 +15,6 @@ public class Common_goal {
 	
 	public int Checkcommon(int card, BookshelfObject[][] library) {
 		int cont = 0; //used to assign points to the first, second, third, fourth place
-		BookshelfObject object0 = null;
-		BookshelfObject object1 = null;
-		BookshelfObject object2 = null;
-		BookshelfObject object3 = null;
-		//BookshelfObject object4 = null;
-		//BookshelfObject object5 = null;
 		
 		switch (card) {
 		case 0:
@@ -34,18 +28,19 @@ public class Common_goal {
 				//}	
 				
 			//}
-			object0 = library[0][0];
-			object1 = library[4][0];
-			object2 = library[0][5];
-			object3 = library[4][5];
+			BookshelfObject object0 = library[0][0];
+			BookshelfObject object1 = library[0][4];
+			BookshelfObject object2 = library[5][0];
+			BookshelfObject object3 = library[0][5];
 			
-			if(object0 != null && object1 != null && object2 !=null && object3 !=null) {
+			if(object0!=null&&object1!=null&&object2!=null&&object3!=null) {
 				
-				if(object0.name()==object1.name() && object1.name()==object2.name() && object2.name()==object3.name()) {
+				if(object0==object1&&object1==object2&&object2==object3) {
 					cont++;
 					
 				}	
 			}
+			
 			break;
 			
 		case 2:
@@ -59,47 +54,8 @@ public class Common_goal {
 			
 			break;
 		case 5:
-			int contCats=0;
-			int contBooks=0;
-			int contGames=0;
-			int contFrames=0;
-			int contTrophy=0;
-			int contPlants=0;
-			
-			for(int row=0; row < library.length; row++){
-				for(int col = 0; col < library[0].length; col++) {
-					
-					 object0 = library[row][col];
-					 
-					 if(object0.name()!=null) {
-						 	
-						 	if(object0.name()=="CAT") {
-						 		contCats++;
-							 
-						 	}else if(object0.name()=="BOOK") {
-						 		contBooks++;
-						 	}else if(object0.name()=="GAMES") {
-						 		contGames++;
-						 	}else if(object0.name()=="FRAME") {
-						 		contFrames++;
-						 	}else if(object0.name()=="TROPHY") {
-						 		contTrophy++;
-						 	}else {
-						 		contPlants++;
-						 	}
-						 		
-						}	
-								
-				
-				}	
-			
-			}
-			
-			if(contCats==8||contBooks==8||contGames==8||contFrames==8||contTrophy==8||contPlants==8)
-				cont++;
 			
 			break;
-
 		case 6:
 			
 			break;
@@ -121,9 +77,14 @@ public class Common_goal {
 			break;
 		}
 		
-		
 		return cont;
 		
+		
+		
+		
+		
+		
+	}
+	
 
-}
 }
