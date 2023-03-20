b1c6f2d4d174a116def13c41e1ed544c3a72b9e7
a.zanetti6 <a.zanetti6@studenti.unibg.it>
2023-03-20 18:53:48 +0100

    Adds Tile class
    This class represents a tile of the game board.
    
    Signed-off-by: a.zanetti6 <a.zanetti6@studenti.unibg.it>

---
+++ src/myshelfie/Tile.java
@@ -0,0 +1,33 @@
+package myshelfie;
+
+public class Tile
+{
+	private TileType type;
+	private BookshelfObject object;
+
+	public Tile(TileType type)
+	{
+		this.type = type;
+	}
+
+	public boolean isUsable(int playersNumber)
+	{
+		int allowedPlayers = type.getPlayersNumber();
+		return (playersNumber >= allowedPlayers) ? true : false;
+	}
+
+	public void addObject(BookshelfObject object)
+	{
+		this.object = object;
+	}
+
+	public void removeObject()
+	{
+		object = null;
+	}
+
+	public boolean isEmpty()
+	{
+		return (object == null) ? true : false;
+	}
+}
