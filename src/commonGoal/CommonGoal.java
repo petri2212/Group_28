e19b133d5e4a757053a37cdc1b0232687329dcbd
Andrei <a.petrisor@studenti.unibg.it>
2023-03-25 15:00:20 +0100

    Creating new package for Commongoal cards
    this package will contain a CommonGoal.java interface, and all common
    cards that will implement the commonGoal interface

---
+++ src/commonGoal/CommonGoal.java
@@ -0,0 +1,20 @@
+package commonGoal;
+
+import myshelfie.BookshelfObject;
+
+public interface CommonGoal {
+	
+	
+	
+	
+	
+	public int Checkcommon(BookshelfObject[][] library);
+	
+	public void Description();
+	/*public void print() {
+		
+		System.out.println("questo è il goal: "+this.val);
+		
+	}*/
+
+}
