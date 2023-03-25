fe056008fd5b28ef9b25baf31f9132079ec7583f
Andrei <a.petrisor@studenti.unibg.it>
2023-03-25 15:31:31 +0100

    test s.Description();

--- src/myshelfie/MyShelfie.java
+++ src/myshelfie/MyShelfie.java
@@ -1,9 +1,14 @@
 package myshelfie;
 
+//import commonGoal.CommonGoal;
+//import commonGoal.A;
+
 public class MyShelfie
 {
 	public static void main(String[] args)
 	{
+		//A a = new A();
+		//a.Description();
 	
 	}
 }
