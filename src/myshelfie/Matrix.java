1af07ffafe7291317a23a097af1b5418da1f7bd6
a.zanetti6 <a.zanetti6@studenti.unibg.it>
2023-03-28 23:49:23 +0200

    Adds Matrix class
    This abstract class represents a generic matrix object that can be extended
    as a game board or a bookshelf or a personal goal.
    
    Signed-off-by: a.zanetti6 <a.zanetti6@studenti.unibg.it>

---
+++ src/myshelfie/Matrix.java
@@ -0,0 +1,43 @@
+package myshelfie;
+
+import java.util.HashMap;
+
+public abstract class Matrix<T> {
+
+	protected final int rows;
+	protected final int cols;
+	protected HashMap<MatrixCoords, T> items = new HashMap<MatrixCoords, T>();
+
+	protected Matrix(int rows, int cols) {
+		this.rows = rows;
+		this.cols = cols;
+	}
+
+	/**
+	 * Returns the object stored at the given coordinates.
+	 *
+	 * @param coords	is the object representing row-column coordinates
+	 * @return
+	 */
+	public T get(MatrixCoords coords) {
+		return items.get(coords);
+	}
+
+	/**
+	 * Getter method for the number of columns in the matrix.
+	 *
+	 * @return the number of columns as an integer
+	 */
+	public int getCols() {
+		return cols;
+	}
+
+	/**
+	 * Getter method for the number of rows in the matrix.
+	 *
+	 * @return the number of rows as an integer
+	 */
+	public int getRows() {
+		return rows;
+	}
+}
