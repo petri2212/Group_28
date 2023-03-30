d5008e8387c1dd582da1430e23451dffcd067fd8
Andrei <a.petrisor@studenti.unibg.it>
2023-03-30 18:08:53 +0200

    change getDescription

--- src/commonGoal/CommonGoals_fourVertical.java
+++ src/commonGoal/CommonGoals_fourVertical.java
@@ -1,22 +1,23 @@
 package commonGoal;
 
+import myshelfie.Bookshelf;
 import myshelfie.BookshelfObject;
 
-public class CommonGoals_fourVertical implements CommonGoal {
+public class CommonGoals_fourVertical extends CommonGoal {
 
 	@Override
-	public int Checkcommon(BookshelfObject[][] library) {
+	public int checkCommon(Bookshelf library) {
 		// TODO Auto-generated method stub
 		return 0;
 	}
 
 	@Override
-	public void Description() {
+	public String getDescription() {
 		// TODO Auto-generated method stub
-		System.out.println("Two groups each containing 4 tiles of\r\n"
+		String desc="Two groups each containing 4 tiles of\r\n"
 				+ "the same type in a 2x2 square. The tiles\r\n"
 				+ "of one square can be different from\r\n"
-				+ "those of the other square.");
+				+ "those of the other square.";
+		return desc;
 	}
-
 }
--- src/commonGoal/CommonGoals_threeColumns.java
+++ src/commonGoal/CommonGoals_threeColumns.java
@@ -1,22 +1,23 @@
 package commonGoal;
 
+import myshelfie.Bookshelf;
 import myshelfie.BookshelfObject;
 
-public class CommonGoals_threeColumns implements CommonGoal {
+public class CommonGoals_threeColumns extends CommonGoal {
 
 	@Override
-	public int Checkcommon(BookshelfObject[][] library) {
+	public int checkCommon(Bookshelf library) {
 		// TODO Auto-generated method stub
 		return 0;
 	}
 
 	@Override
-	public void Description() {
+	public String getDescription() {
 		// TODO Auto-generated method stub
-		System.out.println("Three columns each formed by 6 tiles Five tiles of the same type forming an X.\r\n"
+		String desc="Three columns each formed by 6 tiles Five tiles of the same type forming an X.\r\n"
 				+ "of maximum three different types. One\r\n"
 				+ "column can show the same or a different\r\n"
-				+ "combination of another column.");
+				+ "combination of another column.";
+		return desc;
 	}
-
 }
