88afc213037d3d5037806f67e9736d71ef174bd1
a.zanetti6 <a.zanetti6@studenti.unibg.it>
2023-03-31 19:50:41 +0200

    Renames class CommonGoals_fourVertical
    Signed-off-by: a.zanetti6 <a.zanetti6@studenti.unibg.it>

---
+++ src/commonGoal/CommonGoals_FourVertical.java
@@ -0,0 +1,17 @@
+package commonGoal;
+
+import myshelfie.Bookshelf;
+import myshelfie.BookshelfObject;
+
+public class CommonGoals_FourVertical extends CommonGoal {
+
+	public CommonGoals_FourVertical() {
+		description = "";
+	}
+
+	@Override
+	public int checkCommon(Bookshelf library) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+}
