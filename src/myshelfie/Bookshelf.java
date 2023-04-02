97415b99f8df6ca0ebc0c9c3d260bc92bfc56be8
a.zanetti6 <a.zanetti6@studenti.unibg.it>
2023-04-02 18:18:05 +0200

    Adds bookshelf specific attributes and a new method
    The constants COLS_NUMBER and ROWS_NUMBER, with the method get
    have been added to allow the development of the game goals.
    This class will be completed in the future.
    
    Signed-off-by: a.zanetti6 <a.zanetti6@studenti.unibg.it>

--- src/myshelfie/Bookshelf.java
+++ src/myshelfie/Bookshelf.java
@@ -2,4 +2,11 @@ package myshelfie;
 
 public class Bookshelf {
 
+	public static final int COLS_NUMBER = 5;
+	public static final int ROWS_NUMBER = 6;
+
+	public BookshelfObject get(int r, int c) {
+		// TODO: implement
+		return null;
+	}
 }
