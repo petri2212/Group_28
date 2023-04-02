e971885190294878a7b97a19561702733a8433bb
a.zanetti6 <a.zanetti6@studenti.unibg.it>
2023-04-02 19:12:04 +0200

    Implements CommonGoal_ThreeColumns class
    Signed-off-by: a.zanetti6 <a.zanetti6@studenti.unibg.it>

--- src/commonGoal/CommonGoal_ThreeColumns.java
+++ src/commonGoal/CommonGoal_ThreeColumns.java
@@ -1,8 +1,13 @@
 package commonGoal;
 
 import myshelfie.Bookshelf;
+import myshelfie.BookshelfObject;
+import java.util.ArrayList;
 
 public class CommonGoal_ThreeColumns extends CommonGoal {
+
+	private final int MAX_DIFFERENT_OBJECTS = 3;
+	private final int MAX_VALID_COLUMNS = 3;
 
 	public CommonGoal_ThreeColumns(int playersNumber) {
 		super(playersNumber);
@@ -12,9 +17,40 @@ public class CommonGoal_ThreeColumns extends CommonGoal {
 				+ "combination of another column.";
 	}
 
+	/**
+	 * Returns the points for this common goal if the player has successfully achieved it.
+	 * This method runs trough every column and parse all the rows. If the cell in validation
+	 * contains an object that has never been stored, it is saved in a list.
+	 * After the last row is validated, the algorithm checks if the number of object stored
+	 * is maximum 3. If this is the case a counter is increased and the algorithm clears the
+	 * list of saved objects and proceeds to the next column.
+	 * Once the entire bookshelf is parsed, if the number of valid columns of maximum three
+	 * different objects is 3 than the point is given.
+	 *
+	 * @param bookshelf the player bookshelf under validation
+	 * @return 	  		the number of points gained from the goal check
+	 */
 	@Override
 	public int check(Bookshelf bookshelf) {
-		// TODO Auto-generated method stub
-		return 0;
+		int validColsCount = 0;
+		ArrayList<BookshelfObject> objects = new ArrayList<BookshelfObject>();
+
+		for(int c = 0; c < Bookshelf.COLS_NUMBER; c++) {
+			for(int r = 0; r < Bookshelf.ROWS_NUMBER; r++) {
+				BookshelfObject object = bookshelf.get(r, c);
+
+				if(!objects.contains(object)) {
+					objects.add(object);
+				}
+			}
+
+			if(objects.size() <= MAX_DIFFERENT_OBJECTS) {
+				validColsCount++;
+			}
+
+			objects.clear();
+		}
+
+		return (validColsCount == MAX_VALID_COLUMNS) ? getPoints() : 0;
 	}
 }
