5c8893e4d1954d8934b9382336fcac782edb8b1e
AndreaRusco <a.rusconi4@studenti.unibg.it>
2023-04-05 19:35:53 +0200

    Inplements Bookshelf class

--- src/myshelfie/Bookshelf.java
+++ src/myshelfie/Bookshelf.java
@@ -4,9 +4,56 @@ public class Bookshelf {
 
 	public static final int COLS_NUMBER = 5;
 	public static final int ROWS_NUMBER = 6;
+	private BookshelfObject[][] bookshelfObjects;
+	private final int noEmptyRow=-1;
+	
+	public Bookshelf() {
+		bookshelfObjects = new BookshelfObject[ROWS_NUMBER][COLS_NUMBER];
+	}
+	
+	public void add (int c, BookshelfObject obj) {
+		int r = getFirstEmptyRow(c);
+		
+		if(r != noEmptyRow) {
+			bookshelfObjects[r][c] = obj;
+		}else {
+			System.out.println("invalid cols!");
+		}
+	}
+	
+	public void add (int c, BookshelfObject[] objs) {
+		int lenght = objs.length;
+		int r = getFirstEmptyRow(c);
+		
+		if(r != noEmptyRow && isThereEnoughSpace(r,lenght)) {
+			for (BookshelfObject obj : objs) {
+				bookshelfObjects[r][c] = obj;
+				r++;
+			}
+		}else {
+			System.out.println("invalid cols!");
+		}
+		
+	}
+	
+	private boolean isThereEnoughSpace(int r, int lenght) {
+		int rMaxIndex = ROWS_NUMBER-1;
+		int rIndex = r + lenght;
+		
+		return (rIndex <= rMaxIndex) ? true : false;
+	}
+
+	private int getFirstEmptyRow(int c) {
+		for(int r=0; r<ROWS_NUMBER; r++) {
+			if(bookshelfObjects[r][c] == null) {
+				return r;
+			}
+		}
+		return noEmptyRow;
+	}
 
 	public BookshelfObject get(int r, int c) {
-		// TODO: implement
-		return null;
+		return bookshelfObjects[r][c];
 	}
+	
 }
