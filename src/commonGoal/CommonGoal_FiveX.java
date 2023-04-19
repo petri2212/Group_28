57c01920632f8bbc0364bf99ed51f1302fb8ffd1
AndreaRusco <a.rusconi4@studenti.unibg.it>
2023-04-19 22:48:13 +0200

    First version of the commongoal called FiveX

--- src/commonGoal/CommonGoal_FiveX.java
+++ src/commonGoal/CommonGoal_FiveX.java
@@ -2,9 +2,14 @@ package commonGoal;
 
 import myshelfie.Bookshelf;
 import myshelfie.BookshelfObject;
+import utils.MatrixCoords;
 
 public class CommonGoal_FiveX extends CommonGoal{
-
+	
+	private final int MAX_ROWS = 5;
+	private final int MAX_COLS = 4;
+	private final int MIN_ROWS_AND_COLS = 1;
+	
 	protected CommonGoal_FiveX(int playersNumber) {
 		super(playersNumber);
 		// TODO Auto-generated constructor stub
@@ -13,6 +18,24 @@ public class CommonGoal_FiveX extends CommonGoal{
 	@Override
 	public int check(Bookshelf library) {
 		// TODO Auto-generated method stub
+		/*the difficulties is related to the bookshelfobjects in the bookshelf because
+		 * there could be some empty columns and aint't the rows.
+		 */
+		for(int r = MIN_ROWS_AND_COLS; r < MAX_ROWS; r++)
+			for(int c = MIN_ROWS_AND_COLS; c < MAX_COLS; c++) {
+				BookshelfObject center = library.get(new MatrixCoords(r, c));
+				BookshelfObject upLeft = library.get(new MatrixCoords(r+1, c-1));
+				BookshelfObject upRight = library.get(new MatrixCoords(r+1, c+1));
+				BookshelfObject downLeft = library.get(new MatrixCoords(r-1, c-1));
+				BookshelfObject downRight = library.get(new MatrixCoords(r-1, c+1));
+				
+				if(center != null) {
+					if(center == upLeft && center == upRight && center == downLeft && center == downRight) {
+							return getPoints();
+						}
+				}
+			}
+		
 		return 0;
 	}
 
