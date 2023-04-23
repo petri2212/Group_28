08ce4a03f76001e9d23bbf8798d34a85580f343d
a.zanetti6 <a.zanetti6@studenti.unibg.it>
2023-04-23 20:01:21 +0200

    Implements Player class
    Signed-off-by: a.zanetti6 <a.zanetti6@studenti.unibg.it>

--- src/myshelfie/Player.java
+++ src/myshelfie/Player.java
@@ -1,5 +1,59 @@
 package myshelfie;
 
+/**
+ * Represents a physical player of the game
+ */
 public class Player {
 
+	private String name;
+	private int points;
+	private Bookshelf bookshelf;
+
+	/**
+	 * To construct a player, the name is needed as identifier to allow the user understanding when
+	 * it is his/her turn. A player also holds a bookshelf and has a counter for the game points.
+	 *
+	 * @param name	a String that is the player identifier
+	 */
+	public Player(String name) {
+		this.name = name;
+		bookshelf = new Bookshelf();
+		points = 0;
+	}
+
+	/**
+	 * Returns the name of the player.
+	 *
+	 * @return	a String containing the player name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * Retuns the player bookshelf.
+	 *
+	 * @return	a reference of type Bookshelf pointing to the bookshelf of the player
+	 */
+	public Bookshelf getBookshelf() {
+		return bookshelf;
+	}
+
+	/**
+	 * Returns the points of the player
+	 *
+	 * @return	the points as integer
+	 */
+	public int getPoints() {
+		return points;
+	}
+
+	/**
+	 * Sum the input points to the player's points.
+	 *
+	 * @param points	an integer corresponding to the amount of points to add
+	 */
+	public void addPoints(int points) {
+		this.points += points;
+	}
 }
