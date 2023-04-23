07f9a849294ba9c30af645dda3f1e83b304b5956
a.zanetti6 <a.zanetti6@studenti.unibg.it>
2023-04-23 15:01:07 +0200

    Adds javadoc and exception management to the Matrix class
    To reduce the possibility of error when using this class, the corresponding
    javadoc has been added. For the same reason, in case of negative
    dimensions as input, the class now throws an exception.
    
    Signed-off-by: a.zanetti6 <a.zanetti6@studenti.unibg.it>

--- src/utils/Matrix.java
+++ src/utils/Matrix.java
@@ -2,15 +2,33 @@ package utils;
 
 import java.util.HashMap;
 
+/**
+ * Represents a matrix of generic entries.
+ *
+ * @param <T>	type of the entry objects
+ */
 public abstract class Matrix<T> {
 
 	protected final int rows;
 	protected final int cols;
 	protected HashMap<MatrixCoords, T> items = new HashMap<MatrixCoords, T>();
 
+	/**
+	 * To be constructed, a matrix needs to know its dimensions specified by the rows count and
+	 * the columns count passed as input.
+	 *
+	 * @param rows							is the rows count
+	 * @param cols							is the columns count
+	 * @throws NegativeArraySizeException	if one of the argument is negative
+	 */
 	protected Matrix(int rows, int cols) {
-		this.rows = rows;
-		this.cols = cols;
+		if(rows < 0 || cols < 0) {
+			throw new NegativeArraySizeException("The matrix dimensions cannot be negative.");
+		}
+		else {
+			this.rows = rows;
+			this.cols = cols;
+		}
 	}
 
 	/**
