0c742cb1564bf9b4bec9f30ad0806640cd86b25b
a.zanetti6 <a.zanetti6@studenti.unibg.it>
2023-04-25 21:22:03 +0200

    Adds UI interface
    This interface will hold the list of the methods available to allow the
    user to interact with the program. For the first implementation, this
    interface only contains the method showMainPage as an example.
    
    Signed-off-by: a.zanetti6 <a.zanetti6@studenti.unibg.it>

---
+++ src/gui/UI.java
@@ -0,0 +1,18 @@
+package gui;
+
+import myshelfie.GameManager;
+
+/**
+ * Interface to handle the user interaction with the program both input and output.
+ */
+public interface UI {
+
+	/**
+	 * Visualize the main page.
+	 *
+	 * @param manager	is the reference to the game manager component. It is used to link the
+	 * 					controller to the game manager to allow state changes.
+	 */
+	void showMainPage(GameManager model);
+
+}
