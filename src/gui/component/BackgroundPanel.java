df33c4cdf31d28ee1e5e1d34151b7896b338654a
a.zanetti6 <a.zanetti6@studenti.unibg.it>
2023-04-25 22:24:25 +0200

    Adds BackgroundPanel class
    This class extends from JPanel to allow the panel to have a background
    image.
    
    Signed-off-by: a.zanetti6 <a.zanetti6@studenti.unibg.it>

---
+++ src/gui/component/BackgroundPanel.java
@@ -0,0 +1,38 @@
+package gui.component;
+
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.LayoutManager;
+
+import javax.swing.JPanel;
+
+public class BackgroundPanel extends JPanel {
+
+    private static final long serialVersionUID = 1L;
+	private Image background;
+
+    public BackgroundPanel(Image background) {
+        this.background = background;
+        setLayout(new BorderLayout());
+    }
+
+    public BackgroundPanel(Image background, LayoutManager layout) {
+        this.background = background;
+        setLayout(layout);
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.drawImage(background, 0, 0, null); // image full size
+        //g.drawImage(background, 0, 0, getWidth(), getHeight(), null); // image scaled
+    }
+
+    @Override
+    public Dimension getPreferredSize() {
+        return new Dimension(background.getWidth(this), background.getHeight(this));
+    }
+
+}
