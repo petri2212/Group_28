222398c7f38bf7cc6cb76749c02197ab43955b0e
a.zanetti6 <a.zanetti6@studenti.unibg.it>
2023-04-25 23:04:50 +0200

    Adds GameManager class
    This class is the data holder of the game in progress.
    It makes available a method to change game state and acts consequently.
    The GameManager is responsible to call a new UI page.
    
    Signed-off-by: a.zanetti6 <a.zanetti6@studenti.unibg.it>

---
+++ src/myshelfie/GameManager.java
@@ -0,0 +1,58 @@
+package myshelfie;
+
+import java.util.ArrayList;
+
+import gui.UI;
+
+public class GameManager {
+
+	private GameState state;
+	private UI ui;
+	private ArrayList<Player> players;
+	private Board board;
+	private int playerTurn;
+
+	public GameManager(UI ui) {
+		this.ui = ui;
+	}
+
+	public void start() {
+		state = GameState.START;
+		players = new ArrayList<>();
+		playerTurn = 0;
+
+		manageState();
+	}
+
+	public void changeState(GameState nextState) {
+		state = nextState;
+		manageState();
+	}
+
+	private void manageState() {
+		switch(state) {
+		case START:
+			ui.showMainPage(this);
+			break;
+
+		case NEW_GAME:
+			break;
+
+		case PLAY:
+			board = new Board(players.size());
+			updatePlayerTurn();
+			break;
+
+		case END:
+			break;
+
+		default:
+			break;
+		}
+	}
+
+	private void updatePlayerTurn() {
+		playerTurn = (playerTurn >= players.size() - 1) ? playerTurn++ : 0;
+	}
+
+}
