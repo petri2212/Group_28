7a4a706e6875e89bebfc828824a63a68fdf75cca
a.zanetti6 <a.zanetti6@studenti.unibg.it>
2023-05-21 22:35:49 +0200

    Links the new resources to the code
    Signed-off-by: a.zanetti6 <a.zanetti6@studenti.unibg.it>

--- src/resource/Icons.java
+++ src/resource/Icons.java
@@ -7,7 +7,9 @@ public enum Icons implements Resource<ImageIcon> {
 	ICON_CAT("./resources/icon_tiles/icon_cat.png"),
 	ICON_TROPHY("./resources/icon_tiles/icon_trophy.png"),
 	ICON_GAME("./resources/icon_tiles/icon_game.png"),
-	ICON_PLANT("./resources/icon_tiles/icon_plant.png");
+	ICON_PLANT("./resources/icon_tiles/icon_plant.png"),
+	BOARD("./resources/boards/livingroom_reduced.png"),
+	BOARD_BLURRED("./resources/boards/livingroom_reduced_blurred.png");
 
 	private String path;
 
