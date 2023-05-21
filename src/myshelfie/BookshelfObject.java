bca6e03aabd468fb20ebf9650be577c7d4fc6934
a.zanetti6 <a.zanetti6@studenti.unibg.it>
2023-05-21 22:47:33 +0200

    Adds images to BookshelfObject implementing the Drawable interface
    Signed-off-by: a.zanetti6 <a.zanetti6@studenti.unibg.it>

--- src/myshelfie/BookshelfObject.java
+++ src/myshelfie/BookshelfObject.java
@@ -1,23 +1,35 @@
 package myshelfie;
 
+import java.awt.Image;
 import java.util.Random;
 
-public enum BookshelfObject {
+import gui.DrawableObject;
+import resource.Images;
 
-	CAT(),
-	BOOK(),
-	GAME(),
-	FRAME(),
-	TROPHY(),
-	PLANT();
+public enum BookshelfObject implements DrawableObject {
+
+	CAT(new Images[] { Images.BOOKSHELF_OBJECT_CAT_1, Images.BOOKSHELF_OBJECT_CAT_2,
+			Images.BOOKSHELF_OBJECT_CAT_3 }),
+	BOOK(new Images[] { Images.BOOKSHELF_OBJECT_BOOK_1, Images.BOOKSHELF_OBJECT_BOOK_2,
+			Images.BOOKSHELF_OBJECT_BOOK_3 }),
+	GAME(new Images[] { Images.BOOKSHELF_OBJECT_GAME_1, Images.BOOKSHELF_OBJECT_GAME_2,
+			Images.BOOKSHELF_OBJECT_GAME_3 }),
+	FRAME(new Images[] { Images.BOOKSHELF_OBJECT_FRAME_1, Images.BOOKSHELF_OBJECT_FRAME_2,
+			Images.BOOKSHELF_OBJECT_FRAME_3 }),
+	TROPHY(new Images[] { Images.BOOKSHELF_OBJECT_TROPHY_1, Images.BOOKSHELF_OBJECT_TROPHY_2,
+			Images.BOOKSHELF_OBJECT_TROPHY_3 }),
+	PLANT(new Images[] { Images.BOOKSHELF_OBJECT_PLANT_1, Images.BOOKSHELF_OBJECT_PLANT_2,
+			Images.BOOKSHELF_OBJECT_PLANT_3 });
 
 	public static final int MAX_INSTANCES = 22;
 	private static final Random RANDOM = new Random();
 
 	private int instancesCounter;
+	private Images[] images;
 
-	private BookshelfObject() {
+	private BookshelfObject(Images[] images) {
 		instancesCounter = 0;
+		this.images = images;
 	}
 
 	private boolean reachedMaxInstances() {
@@ -48,6 +60,12 @@ public enum BookshelfObject {
 
 		object.instancesCounter++;
 		return object;
+	}
+
+	@Override
+	public Image getImage() {
+		int randomIndex = RANDOM.nextInt(images.length - 1);
+		return images[randomIndex].load();
 	}
 
 }
