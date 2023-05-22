18f177abbc2352ca9a9a26dc74b0ff0fb2964edb
Fabio Mariani <f.mariani9@studenti.unibg.it>
2023-05-22 23:00:27 +0200

    added method readfile into SaveManager.java

--- src/resource/SaveManager.java
+++ src/resource/SaveManager.java
@@ -1,5 +1,33 @@
 package resource;
 
-public class SaveManager {
+import java.io.*;
 
+public class SaveManager {
+	
+	String File="./resources/game_saves/save.txt";
+	
+	
+	/*
+	 * method used to read the file
+	 */
+	
+	
+	
+	public static String readfile(BufferedReader br, int n) throws IOException {
+		String line="";
+		
+		for(int i=0;i<n;i++) {
+			try {
+				line=br.readLine();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			if(line==null)
+				break;
+		}
+		return line;
+}
+	
 }
