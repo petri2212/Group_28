0698d6b857d1a3de2a19b4cea4b918f59f69390f
a.zanetti6 <a.zanetti6@studenti.unibg.it>
2023-05-27 14:45:40 +0200

    Removes warning
    Signed-off-by: a.zanetti6 <a.zanetti6@studenti.unibg.it>

--- src/gui/view/graphic/GameStageViewGraphic.java
+++ src/gui/view/graphic/GameStageViewGraphic.java
@@ -10,7 +10,6 @@ import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.util.ArrayList;
 
-import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
