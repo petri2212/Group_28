6ec3443b2640586b03b4742e0a8f235581dd141b
Andrei <a.petrisor@studenti.unibg.it>
2023-05-29 16:06:51 +0200

    Final implementation
    Co-Author: <a.rusconi4@studenti.unibg.it>

--- src/goal/common/CommonGoal_Diagonal.java
+++ src/goal/common/CommonGoal_Diagonal.java
@@ -1,7 +1,6 @@
 package goal.common;
 
 import java.awt.Image;
-import java.util.ArrayList;
 
 import goal.CommonGoal;
 import myshelfie.Bookshelf;
@@ -29,16 +28,11 @@ public class CommonGoal_Diagonal extends CommonGoal {
 		 * them start form bottom left and end top right and the other two start from
 		 * bottom right and end top left.
 		 */
-		/*
-		 * This CommonGoal has only four possible tile to start from, because two of
-		 * them start form bottom left and end top right and the other two start from
-		 * bottom right and end top left.
-		 */
 
 		boolean value = false;
 		BookshelfObject next;
-		int r = FRIST_COL_OR_ROW;
-		int c = FRIST_COL_OR_ROW;
+		int r = 0;
+		int c = 0;
 
 		BookshelfObject downLeft = library.get(new MatrixCoords(FRIST_COL_OR_ROW, FRIST_COL_OR_ROW));
 		BookshelfObject upLeft = library.get(new MatrixCoords(SECOND_ROW, FRIST_COL_OR_ROW));
@@ -46,18 +40,21 @@ public class CommonGoal_Diagonal extends CommonGoal {
 		BookshelfObject upRight = library.get(new MatrixCoords(SECOND_ROW, SECOND_COL));
 
 		if (downLeft != null) {
+			r = FRIST_COL_OR_ROW;
+			c = FRIST_COL_OR_ROW;
 			for (int i = 1; i < DIAGONAL_LENGHT; i++) {
-				try {
-					next = library.get(new MatrixCoords(r + i, c + i));
-				} catch (Exception e) {
-					value = false;
-					break;
-				}
-				if (downLeft.name() != next.name()) {
-					value = false;
-					break;
+
+				next = library.get(new MatrixCoords(r + i, c + i));
+				if (next != null) {
+					if (downLeft.name() != next.name()) {
+						value = false;
+						break;
+					} else {
+						value = true;
+					}
 				} else {
-					value = true;
+					value = false;
+					break;
 				}
 			}
 			if (value) {
@@ -67,63 +64,79 @@ public class CommonGoal_Diagonal extends CommonGoal {
 		}
 
 		if (upLeft != null) {
+			r = SECOND_ROW;
+			c = FRIST_COL_OR_ROW;
 			for (int i = 1; i < DIAGONAL_LENGHT; i++) {
-				try {
-					next = library.get(new MatrixCoords(r + i, c + i));
-				} catch (Exception e) {
-					value = false;
-					break;
-				}
-				if (downLeft.name() != next.name()) {
-					value = false;
-					break;
+
+				next = library.get(new MatrixCoords(r + i, c + i));
+
+				if (next != null) {
+					if (upLeft.name() != next.name()) {
+						value = false;
+						break;
+					} else {
+						value = true;
+					}
 				} else {
-					value = true;
+					value = false;
+					break;
 				}
 			}
+
 			if (value) {
 				return 1;
 			}
+
 		}
 
 		if (downRight != null) {
+			r = FRIST_COL_OR_ROW;
+			c = SECOND_COL;
 			for (int i = 1; i < DIAGONAL_LENGHT; i++) {
-				try {
-					next = library.get(new MatrixCoords(r + i, c - i));
-				} catch (Exception e) {
-					value = false;
-					break;
-				}
-				if (downLeft.name() != next.name()) {
-					value = false;
-					break;
+
+				next = library.get(new MatrixCoords(r + i, c - i));
+
+				if (next != null) {
+					if (downRight.name() != next.name()) {
+						value = false;
+						break;
+					} else {
+						value = true;
+					}
 				} else {
-					value = true;
+					value = false;
+					break;
 				}
 			}
 			if (value) {
 				return 1;
 			}
+
 		}
 
 		if (upRight != null) {
+			r = SECOND_ROW;
+			c = SECOND_COL;
 			for (int i = 1; i < DIAGONAL_LENGHT; i++) {
-				try {
-					next = library.get(new MatrixCoords(r + i, c - i));
-				} catch (Exception e) {
-					value = false;
-					break;
-				}
-				if (downLeft.name() != next.name()) {
-					value = false;
-					break;
+
+				next = library.get(new MatrixCoords(r + i, c - i));
+
+				if (next != null) {
+					if (upRight.name() != next.name()) {
+						value = false;
+						break;
+					} else {
+						value = true;
+					}
 				} else {
-					value = true;
+					value = false;
+					break;
 				}
 			}
 			if (value) {
 				return 1;
 			}
+
 		}
 
 		return 0;
