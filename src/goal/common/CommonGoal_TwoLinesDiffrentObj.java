dce3a8f597cbf671cf75085a8e8d326059525d0a
Andrei <a.petrisor@studenti.unibg.it>
2023-05-29 17:45:44 +0200

    Debugging TwoLinesDifferentObj

--- src/goal/common/CommonGoal_TwoLinesDiffrentObj.java
+++ src/goal/common/CommonGoal_TwoLinesDiffrentObj.java
@@ -8,8 +8,8 @@ import myshelfie.BookshelfObject;
 import resource.Images;
 import utils.MatrixCoords;
 
-public class CommonGoal_TwoLinesDiffrentObj extends CommonGoal{
-	
+public class CommonGoal_TwoLinesDiffrentObj extends CommonGoal {
+
 	private static final int ROWS_NUMBER = 6;
 	private static final int COLS_NUMBER = 5;
 	private static final int ROWS_DIFERRENT = 2;
@@ -22,26 +22,28 @@ public class CommonGoal_TwoLinesDiffrentObj extends CommonGoal{
 	@Override
 	public int check(Bookshelf library) {
 		int cont = 0;
-		int tmpCols = 0;
-		
-		for(int r = 0; r < ROWS_NUMBER; r++) {
-			if(library.fullCol(r)) {
-				for(int c = 0; c < COLS_NUMBER; c++) {
-					BookshelfObject tmpVar = library.get(new MatrixCoords(r, c));
-					do {
-						if(tmpCols != c) {
-							if(tmpVar == library.get(new MatrixCoords(r, tmpCols))) {
-								break;
-							}
+		int tmpCols = 1;
+		int col = 0;
+
+		for (int r = 0; r < ROWS_NUMBER; r++) {
+			if (library.fullCol(r)) {
+				for (int c = 0; c < COLS_NUMBER - 1; c++) {
+					BookshelfObject tmpVar = library.get(new MatrixCoords(r, col));
+					BookshelfObject next = library.get(new MatrixCoords(r, tmpCols));
+					if (tmpVar != null && next != null) {
+						if (tmpVar.name() != next.name()) {
+							tmpCols++;
+						} else {
+							break;
 						}
-						tmpCols++;
-					}while(tmpCols < COLS_NUMBER);
-					if(tmpCols == COLS_NUMBER) {
-						cont++;
 					}
 				}
+				if (tmpCols == COLS_NUMBER) {
+					cont++;
+				}
+				tmpCols = 1;
 			}
-			if(cont == ROWS_DIFERRENT) {
+			if (cont >= ROWS_DIFERRENT) {
 				return 1;
 			}
 		}
@@ -51,10 +53,8 @@ public class CommonGoal_TwoLinesDiffrentObj extends CommonGoal{
 	@Override
 	public String getDescription() {
 		// TODO Auto-generated method stub
-		String desc="Two lines each formed by 5 different\r\n"
-				+ "types of tiles. One line can show the\r\n"
-				+ "same or a different combination of the\r\n"
-				+ "other line.";
+		String desc = "Two lines each formed by 5 different\r\n" + "types of tiles. One line can show the\r\n"
+				+ "same or a different combination of the\r\n" + "other line.";
 		return desc;
 	}
 }
