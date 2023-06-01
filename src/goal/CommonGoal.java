f7c182c638aaa9a08983d811cabecc277a500c4c
a.zanetti6 <a.zanetti6@studenti.unibg.it>
2023-06-01 16:35:03 +0200

    Overrides CommonGoal and PersonalGoal equal method to work with UniqueList
    In this way we can be sure that there are no doubles inside the decks that
    collect the goals.
    
    Signed-off-by: a.zanetti6 <a.zanetti6@studenti.unibg.it>

--- src/goal/CommonGoal.java
+++ src/goal/CommonGoal.java
@@ -8,4 +8,14 @@ public abstract class CommonGoal implements Goal, DrawableObject {
 	 * @return the common goal description
 	 */
 	public abstract String getDescription();
+
+	/**
+	 * Returns true if the confronted CommonGoal has the same name.
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		String name = this.getClass().getName();
+		String objName = obj.getClass().getName();
+		return name.equals(objName);
+	}
 }
