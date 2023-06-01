7b4bc1b2eff84e41d1fc6a2762eb7d30f80b9a39
Andrei <a.petrisor@studenti.unibg.it>
2023-06-01 12:05:52 +0200

    Implementing genericGoal

--- src/goal/GenericGoal.java
+++ src/goal/GenericGoal.java
@@ -1,29 +1,222 @@
 package goal;
 
 import java.util.HashSet;
-import java.util.Iterator;
+import java.util.ArrayList;
 
 import myshelfie.Bookshelf;
 import myshelfie.BookshelfObject;
+import utils.MatrixCoords;
 
 public class GenericGoal implements Goal {
 
 	@Override
 	public int check(Bookshelf bookshelf) {
-		
-		
-		for (int r = 0; r < bookshelf.getRows(); r++) {
-			HashSet<BookshelfObject> tmpRow = new HashSet<>();
+		int points = 0;
 
-			for (int c = 0; c < bookshelf.getCols(); c++) {
-				
+		BookshelfObject[] items_to_check = { BookshelfObject.CAT, BookshelfObject.BOOK, BookshelfObject.GAME,
+				BookshelfObject.FRAME, BookshelfObject.TROPHY, BookshelfObject.PLANT };
+		ArrayList<MatrixCoords> Coordinates_Array = new ArrayList<>();
+		ArrayList<Integer> Num_obj_for_tipe = new ArrayList<>();
+
+		for (int i = 0; i < items_to_check.length; i++) {
+			BookshelfObject actualObj = items_to_check[i];
+			for (int r = 0; r < bookshelf.getRows(); r++) {
+				for (int c = 0; c < bookshelf.getCols(); c++) {
+					BookshelfObject acutal = bookshelf.get(new MatrixCoords(r, c));
+					if (acutal != null && actualObj.name() == acutal.name()) {
+						ricorsive(new MatrixCoords(r, c), Coordinates_Array, bookshelf, actualObj);
+
+					}
+				}
 			}
-			
-		}
-		
-		
 
-		return 0;
+			if (Coordinates_Array.size() != 0) {
+
+				ArrayList<MatrixCoords> CoordsCopied = new ArrayList<>(Coordinates_Array);
+				int pos = 0;
+				int cont = 1;
+
+				for (MatrixCoords next : Coordinates_Array) {
+
+					boolean isEnd = false;
+					CoordsCopied.remove(pos);
+					if (!CoordsCopied.isEmpty()) {
+						for (MatrixCoords cop : CoordsCopied) {
+
+							if (next.r != cop.r || next.c != cop.c) {
+
+								if (next.r == cop.r && next.c == cop.c + 1) {
+									cont++;
+									break;
+								} else if (next.r == cop.r && next.c == cop.c - 1) {
+									cont++;
+									break;
+								} else if (next.r == cop.r + 1 && next.c == cop.c) {
+									cont++;
+									break;
+								} else if (next.r == cop.r - 1 && next.c == cop.c) {
+									cont++;
+									break;
+								} else {
+									isEnd = true;
+									break;
+								}
+							}
+						}
+
+					} else {
+						isEnd = true;
+					}
+
+					if (isEnd == true) {
+						if (cont >= 3) {
+							Num_obj_for_tipe.add(cont);
+							cont = 1;
+						}
+						cont = 1;
+
+					}
+
+				}
+			}
+			Coordinates_Array.clear();
+
+		}
+
+		for (Integer cont : Num_obj_for_tipe) {
+			if (cont == 3) {
+				points += 2;
+			} else if (cont == 4) {
+				points += 3;
+			} else if (cont == 5) {
+				points += 5;
+			} else if (cont >= 6) {
+				points += 8;
+			}
+		}
+
+		return points;
 	}
 
+	public static void ricorsive(MatrixCoords actualCoords, ArrayList<MatrixCoords> Coordinate, Bookshelf bookshelf,
+			BookshelfObject actualObj) {
+		int flag = 0;
+		int flag2 = 0;
+		int flag3 = 0;
+		int flag4 = 0;
+		int jump_already_present = 0;
+		int jump_already_present2 = 0;
+		int jump_already_present3 = 0;
+		int jump_already_present4 = 0;
+		
+		int Right_col = actualCoords.c + 1;
+		BookshelfObject obj_d = bookshelf.get(new MatrixCoords(actualCoords.r, actualCoords.c + 1));
+		if (obj_d != null && obj_d.name() == actualObj.name()) {
+
+			for (MatrixCoords mat_coords : Coordinate) {
+				if (mat_coords.r == actualCoords.r && mat_coords.c == actualCoords.c + 1) {
+					flag = 1;
+				}
+
+			}
+			for (MatrixCoords mat_coords : Coordinate) {
+				if (mat_coords.r == actualCoords.r && mat_coords.c == actualCoords.c) {
+					jump_already_present = 1;
+				}
+
+			}
+			if (jump_already_present == 0) {
+				Coordinate.add(new MatrixCoords(actualCoords.r, actualCoords.c));
+			}
+			if (flag != 1) {
+
+				Coordinate.add(new MatrixCoords(actualCoords.r, Right_col));
+				ricorsive(new MatrixCoords(actualCoords.r, Right_col), Coordinate, bookshelf, actualObj);
+			}
+		}
+	
+		
+		int Up_row = actualCoords.r + 1;
+		BookshelfObject obj_su = bookshelf.get(new MatrixCoords(Up_row, actualCoords.c));
+		if (obj_su != null && obj_su.name() == actualObj.name()) {
+			for (MatrixCoords mat_coords : Coordinate) {
+				if (mat_coords.r == Up_row && mat_coords.c == actualCoords.c) {
+					flag2 = 1;
+				}
+
+			}
+			for (MatrixCoords mat_coords : Coordinate) {
+				if (mat_coords.r == actualCoords.r && mat_coords.c == actualCoords.c) {
+					jump_already_present2 = 1;
+				}
+
+			}
+			if (jump_already_present2 == 0) {
+				Coordinate.add(new MatrixCoords(actualCoords.r, actualCoords.c));
+			}
+			if (flag2 != 1) {
+				Coordinate.add(new MatrixCoords(Up_row, actualCoords.c));
+				ricorsive(new MatrixCoords(Up_row, actualCoords.c), Coordinate, bookshelf, actualObj);
+			}
+
+		}
+
+	
+		int R_giu = actualCoords.r - 1;
+		if (R_giu < 0) {
+			R_giu = 10;
+		}
+		BookshelfObject obj_giu = bookshelf.get(new MatrixCoords(R_giu, actualCoords.c));
+		if (obj_giu != null && obj_giu.name() == actualObj.name()) {
+			for (MatrixCoords mat_coords : Coordinate) {
+
+				if (mat_coords.r == R_giu && mat_coords.c == actualCoords.c) {
+					flag3 = 1;
+				}
+
+			}
+			for (MatrixCoords mat_coords : Coordinate) {
+				if (mat_coords.r == actualCoords.r && mat_coords.c == actualCoords.c) {
+					jump_already_present3 = 1;
+				}
+
+			}
+			if (jump_already_present3 == 0) {
+				Coordinate.add(new MatrixCoords(actualCoords.r, actualCoords.c));
+			}
+
+			if (flag3 != 1) {
+				Coordinate.add(new MatrixCoords(R_giu, actualCoords.c));
+				ricorsive(new MatrixCoords(R_giu, actualCoords.c), Coordinate, bookshelf, actualObj);
+			}
+		}
+
+		int Left = actualCoords.c - 1;
+		if (Left < 0) {
+			Left = 10;
+		}
+		BookshelfObject obj_left = bookshelf.get(new MatrixCoords(actualCoords.r, Left));
+		if (obj_left != null && obj_left.name() == actualObj.name()) {
+			for (MatrixCoords mat_coords : Coordinate) {
+
+				if (mat_coords.r == actualCoords.r && mat_coords.c == Left) {
+					flag4 = 1;
+				}
+			}
+			for (MatrixCoords mat_coords : Coordinate) {
+				if (mat_coords.r == actualCoords.r && mat_coords.c == actualCoords.c) {
+					jump_already_present4 = 1;
+				}
+
+			}
+			if (jump_already_present4 == 0) {
+				Coordinate.add(new MatrixCoords(actualCoords.r, actualCoords.c));
+			}
+
+			if (flag4 != 1) {
+				Coordinate.add(new MatrixCoords(actualCoords.r, Left));
+				ricorsive(new MatrixCoords(actualCoords.r, Left), Coordinate, bookshelf, actualObj);
+			}
+		}
+	}
 }
