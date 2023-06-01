4884c1ef0963da0eeb2c2476af4ee1736e401b78
Andrei <a.petrisor@studenti.unibg.it>
2023-06-02 00:16:27 +0200

    Update the version of the game

--- src/myshelfie/MyShelfie.java
+++ src/myshelfie/MyShelfie.java
@@ -7,13 +7,13 @@ import gui.*;
  */
 public class MyShelfie {
 
-	public static final String VERSION = "0.0";
+	public static final String VERSION = "1.0";
 	public static final int MIN_PLAYERS = 2;
 	public static final int MAX_PLAYERS = 4;
 
 	public static void main(String[] args) {
 
-		// UI ui = new ConsoleUI();
+		//UI ui = new ConsoleUI();
 		UI ui = new GraphicUI();
 
 		GameManager gm = new GameManager(ui);
